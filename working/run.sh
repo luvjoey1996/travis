88edc8017f4103ded0509b166e985f8f45e10fbc
luvjoey1996 <luvjoey1996@gmail.com>
2022-01-07 00:39:59 +0800

    fix moving file

--- working/run.sh
+++ working/run.sh
@@ -2,5 +2,6 @@
 
 cd /root/working
 livemedia-creator --make-tar --no-virt --nomacboot --iso=/root/working/boot.iso \
-  --image-name=tos-7.4.1708-docker.tar.xz --project="Centos 7 Docker" --releasever="7" \
+  --image-name=centos-7.4.1708-docker.tar.xz --project="Centos 7 Docker" --releasever="7" \
   --ks=/root/working/kickstart.ks
+mv /var/tmp/centos-7.4.1708-docker.tar.xz /root/working
